3814513be184af1943fc2968eff480b011a9329c
Flex <flextoutcourt@gmail.com>
2026-01-06 10:45:57 +0100

    Add Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,8 +93,28 @@ export default function RootLayout({
             "permissions": "browser"
           })
         }} />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N8NJQMTZ');`,
+          }}
+        />
       </head>
       <body className="bg-slate-900 text-gray-100 min-h-screen flex flex-col antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8NJQMTZ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Providers>
           
           {/* Modern Navigation */}
